bedb45c4866ed7d0a56b69ad90ec66b7ba916b0f
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-02-12 22:17:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# 8
+f8 = lambda asos, balandlik: (
+    0.5
+    * asos
+    * balandlik
+)
+
+# 9
+f9 = lambda x: (
+    True
+    if x % 2 == 0
+    else False
+)
+
+# 10
+f10 = lambda x: (
+    True
+    if x > 10
+    else False
+)
+
+# 11
+f11 = lambda name: (
+    True
+    if "a" in name
+    else False
+)
+
+# 12
+f12 = lambda s: (
+    s.upper()
+)
+
+# 13
+f13 = lambda a, b: (
+    abs(
+        a - b
+    )
+)
+
+# 14
+f14 = lambda a, b: (
+    True
+    if a == b
+    else False
+)
